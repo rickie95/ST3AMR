aaa11109416d0fa4e9423cc3b6443d4997c5c8a2
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2023-04-24 22:37:30 +0200

    adds one more hidden layer for both ENC and DEC

--- autoencoder.py
+++ autoencoder.py
@@ -8,25 +8,37 @@ class AutoEncoder(nn.Module):
 
     def __init__(self, **kwargs):
         super().__init__()
-        self.encoder_hidden_layer = nn.Linear(
+        self.encoder_input_layer = nn.Linear(
             in_features=kwargs["input_shape"], out_features=128
         )
-        self.encoder_output_layer = nn.Linear(
-            in_features=128, out_features=128
+        self.encoder_hidden_layer_1 = nn.Linear(
+            in_features=128, out_features=64
         )
-        self.decoder_hidden_layer = nn.Linear(
-            in_features=128, out_features=128
+        self.encoder_hidden_layer_2 = nn.Linear(
+            in_features=64, out_features=32
+        )
+        self.decoder_hidden_layer_1 = nn.Linear(
+            in_features=32, out_features=64
+        )
+        self.decoder_hidden_layer_2 = nn.Linear(
+            in_features=64, out_features=128
         )
         self.decoder_output_layer = nn.Linear(
             in_features=128, out_features=kwargs["input_shape"]
         )
 
     def forward(self, features):
-        activation = self.encoder_hidden_layer(features)
+        activation = self.encoder_input_layer(features)
         activation = torch.relu(activation)
-        code = self.encoder_output_layer(activation)
+        activation = self.encoder_hidden_layer_1(activation)
+        activation = torch.relu(activation)
+        code = self.encoder_hidden_layer_2(activation)
+
         code = torch.sigmoid(code)
-        activation = self.decoder_hidden_layer(code)
+
+        activation = self.decoder_hidden_layer_1(code)
+        activation = torch.relu(activation)
+        activation = self.decoder_hidden_layer_2(activation)
         activation = torch.relu(activation)
         activation = self.decoder_output_layer(activation)
         reconstructed = torch.sigmoid(activation)
