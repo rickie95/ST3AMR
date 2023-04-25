6cc63c01d8580b86974ae227b2f4439f0efe1e1c
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2023-04-25 12:18:33 +0200

    adds some kind of validation step

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torchvision
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+import torch.utils.data as torch_utils
 
 from PIL import Image
 
@@ -18,9 +19,11 @@ def main():
     device = torch.device("cuda")
 
     model = ConvolutionalAutoEncoder().to(device)
+    print(model)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     criterion = nn.MSELoss()
-    epochs = 40 
+    epochs = 40
+    validation_split = 0.1
 
     # Load MNIST data
     logging.info("Downloading MNIST dataset.")
@@ -31,47 +34,84 @@ def main():
         root="./datasets/MNIST", train=True, transform=transform, download=True
     )
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True
+    dataset_len = len(train_dataset)
+    indices = list(range(dataset_len))
+
+    # Randomly splitting indices:
+    val_len = int(np.floor(validation_split * dataset_len))
+    validation_idx = np.random.choice(indices, size=val_len, replace=False)
+    train_idx = list(set(indices) - set(validation_idx))
+
+    train_sampler = torch_utils.SubsetRandomSampler(train_idx)
+    validation_sampler = torch_utils.SubsetRandomSampler(validation_idx)
+
+
+    train_loader = torch_utils.DataLoader(
+        train_dataset, batch_size=128, num_workers=4, pin_memory=True, sampler=train_sampler
     )
+
+    validation_loader = torch_utils.DataLoader(
+        train_dataset, batch_size=128, num_workers=4, pin_memory=True, sampler=validation_sampler
+    )
+
+    data_loaders = {
+        'train' : train_loader,
+        'val' : validation_loader
+    }
+
+    losses = {
+        'train' : 0,
+        'val' : 0
+    }
 
     # Traning
     logging.info("Starting training phase...")
 
     for epoch in range(epochs):
-        loss = 0
-        for batch_features, _ in train_loader:
-            # reshape mini-batch data to [N, 784] matrix
-            # load it to the active device
-            batch_features = batch_features.view(-1, 784).to(device)
-            
-            # reset the gradients back to zero
-            # PyTorch accumulates gradients on subsequent backward passes
-            optimizer.zero_grad()
-            
-            # compute reconstructions
-            outputs = model(batch_features)
-            
-            # compute training reconstruction loss
-            train_loss = criterion(outputs, batch_features)
-            
-            # compute accumulated gradients
-            train_loss.backward()
-            
-            # perform parameter update based on current gradients
-            optimizer.step()
-            
-            # add the mini-batch training loss to epoch loss
-            loss += train_loss.item()
         
-        # compute the epoch training loss
-        loss = loss / len(train_loader)
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                #optimizer = optim.scheduler(optimizer, epoch)
+                model.train(True)
+            else:
+                model.train(False)
+
+            losses[phase] = 0
+            loss = 0
+
+            for batch_features, _ in data_loaders[phase]:
+                # reshape mini-batch data to [N, 784] matrix
+                # load it to the active device
+                batch_features = batch_features.to(device)
+                
+                # reset the gradients back to zero
+                # PyTorch accumulates gradients on subsequent backward passes
+                optimizer.zero_grad()
+                
+                # compute reconstructions
+                outputs = model(batch_features)
+                
+                # compute training reconstruction loss
+                train_loss = criterion(outputs, batch_features)
+                
+                if phase == 'train':
+                    # compute accumulated gradients
+                    train_loss.backward()
+                    
+                    # perform parameter update based on current gradients
+                    optimizer.step()
+                
+                # add the mini-batch training loss to epoch loss
+                loss += train_loss.item()
+            
+            # compute the epoch training loss
+            losses[phase] = loss / len(data_loaders[phase])
         
         # display the epoch training loss
-        logging.info("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
+        logging.info("epoch : {}/{}, train loss = {:.6f}, val loss = {:.6f}".format(epoch + 1, epochs, losses['train'], losses['val']))
 
     
-    torch.save(model.state_dict(), "./parameters.ckpts")
+    torch.save(model.state_dict(), "./checkpoints/parameters.ckpts")
 
     # Test the encoder with some example
     logging.info("Testing.....")
